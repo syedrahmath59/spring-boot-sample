b1da4c297688871e4e2b72d87cb7690ab96ffa05
Syed Rahmath <syedrahmath59@gmail.com>
2019-01-18 12:07:50 -0800

    controller updated

--- src/main/java/sample/city/CityController.java
+++ src/main/java/sample/city/CityController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/** 
+ * 
+ * @author user
+ *
+ */
 @RestController
 @Transactional
 public class CityController {
@@ -18,7 +23,10 @@ public class CityController {
     public CityController(CityDao dao) {
         this.dao = dao;
     }
-
+    /**
+     * 
+     * @return
+     */
     @RequestMapping("/")
     public List<City> selectAll() {
         return dao.selectAll();
