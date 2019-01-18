a0acbe783530458613d5c010ed84797eb4987b3f
Syed Rahmath <syedrahmath59@gmail.com>
2019-01-18 12:21:30 -0800

    kkk

--- src/main/java/sample/city/CityController.java
+++ src/main/java/sample/city/CityController.java
@@ -7,7 +7,11 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * 
+ * @author syedrahmath
+ * 
+ */
 @RestController
 @Transactional
 public class CityController {
@@ -23,7 +27,12 @@ public class CityController {
     public List<City> selectAll() {
         return dao.selectAll();
     }
-
+    /**
+     * 
+     * @param id
+     * @param name
+     * @return
+     */
     @RequestMapping("/update")
     public List<City> updateAndSelectAll(
             @RequestParam(value = "id", defaultValue = "1") int id,
