2fd18a4e3250cdd5711769b68bb61b65b43b1fa0
Syed Rahmath <syedrahmath59@gmail.com>
2019-01-18 11:33:14 -0800

    added setter and getter to city class

--- src/main/java/sample/city/City.java
+++ src/main/java/sample/city/City.java
@@ -16,4 +16,22 @@ public class City {
         return "City [id=" + id + ", name=" + name + "]";
     }
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+    
+    
+
 }
